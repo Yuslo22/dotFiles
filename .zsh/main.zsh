761a992546668cde45a012aa310f6a655ca0b405
David Plugge <git@davidplugge.de>
2021-12-02 13:25:46 +0100

    add kubernetes

--- .zsh/main.zsh
+++ .zsh/main.zsh
@@ -24,3 +24,10 @@ if [ ! -z "$theme" ]; then
         source ~/.zsh/themes/$theme.zsh-theme
     fi;
 fi
+
+# load android sdk
+export PATH=$HOME/Applications/android/cmdline-tools/5.0/bin:$PATH
+# load flutter
+export PATH=$HOME/Applications/flutter/bin:$PATH
+# chrome exe
+export CHROME_EXECUTABLE="/mnt/c/Program Files/Google/Chrome/Application/chrome.exe"
--- .zshrc
+++ .zshrc
@@ -5,3 +5,7 @@ source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 plugins=*
 theme=agnoster
 source ~/.zsh/main.zsh
+
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$(go env GOPATH)
+export PATH=$PATH:$GOPATH/bin

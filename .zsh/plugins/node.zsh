e0866822d395b9740d76361240ed073b27d9b633
David Plugge <git@davidplugge.de>
2022-06-27 09:14:07 +0200

    add pnpm config

--- .zsh/plugins/node.zsh
+++ .zsh/plugins/node.zsh
@@ -3,5 +3,9 @@ alias delete-nodemodules="find . -name 'node_modules' -type d -prune -print -exe
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# pnpm
+export PNPM_HOME="/home/$USER/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"

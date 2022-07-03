96867c56f765d066b198792e7787c1e62ffbb664
David Plugge <git@davidplugge.de>
2022-07-03 12:13:33 +0200

    replace nvm with fnm

--- .zsh/plugins/node.zsh
+++ .zsh/plugins/node.zsh
@@ -1,10 +1,11 @@
 # delete all node_modules
 alias delete-nodemodules="find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \;"
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+# fnm
+export FNM_DIR="$HOME/.fnm"
+export PATH=$FNM_DIR:$PATH
+[ -s "$FNM_DIR/completion" ] && \. "$FNM_DIR/completion" # This loads nvm bash_completion
+eval "$(fnm env --use-on-cd)"
 
 # pnpm
 export PNPM_HOME="/home/$USER/.local/share/pnpm"

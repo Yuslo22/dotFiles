f11cb3b7e635d45de4d152270d4252ae96f3126f
David Plugge <git@davidplugge.de>
2022-07-07 17:09:09 +0200

    add pnpm aliases

--- .zsh/plugins/node.zsh
+++ .zsh/plugins/node.zsh
@@ -1,6 +1,3 @@
-# delete all node_modules
-alias delete-nodemodules="find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \;"
-
 # fnm
 export FNM_DIR="$HOME/.fnm"
 export PATH=$FNM_DIR:$PATH
@@ -10,3 +7,15 @@ eval "$(fnm env --use-on-cd)"
 # pnpm
 export PNPM_HOME="/home/$USER/.local/share/pnpm"
 export PATH="$PNPM_HOME:$PATH"
+
+# delete all node_modules
+alias delete-nodemodules="find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \;"
+
+# pnpm
+alias p="pnpm"
+alias pd="pnpm dev"
+alias pb="pnpm build"
+alias pi="pnpm install"
+alias pa="pnpm add"
+alias pad="pnpm add --save-dev"
+alias pu="pnpm up"
